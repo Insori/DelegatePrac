ec46f22299bcc8a35e04ed3589577062262ece6f
Insori <s2110@e-mirim.hs.kr>
2023-08-28 11:02:57 +0900

    람다를 활용한 구현 방법

--- DelegatePrac/Program.cs
+++ DelegatePrac/Program.cs
@@ -12,6 +12,11 @@ namespace DelegatePrac
         {
             public string Name { get; set; }
             public int Price { get; set; }
+
+            public override string ToString()
+            {
+                return this.Name + " : " + this.Price;
+            }
         }
         static void Main(string[] args)
         {
@@ -28,17 +33,25 @@ namespace DelegatePrac
             //products.Sort(SortWithPrice);
 
             //무명 Delegator를 활용한 구현 방법
-            products.Sort(delegate (Product x, Product y)
-            {
-                return x.Price.CompareTo(y.Price);
-            });
+            //products.Sort(delegate (Product x, Product y)
+            //{
+            //    return x.Price.CompareTo(y.Price);
+            //});
 
+            //람다를 활용한 구현 방법1
+            //products.Sort((x, y) => { return x.Price.CompareTo(y.Price); });
+
+            //람다를 활용한 구현 방법2
+            products.Sort((x, y) => x.Price.CompareTo(y.Price));
+
+            //아래 출력과 같음
+            products.ForEach(p => Console.WriteLine(p.Name + " : " + p.Price));
 
             //출력
-            foreach (var item in products)
-            {
-                Console.WriteLine(item.Name + " : " + item.Price);
-            }
+            //foreach (var item in products)
+            //{
+            //    Console.WriteLine(item.Name + " : " + item.Price);
+            //}
         }
 
         private static int SortWithPrice(Product x, Product y)
